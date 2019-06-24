75df282d7369dcd8effe2b905b1481abd58ecff4
Habiba Mansouri <habiba.mansouri@altyx.io>
2019-06-24 15:11:36 +0100

    BO - mapper les entités par rapport aux tables

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,19 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/index", name="index")
+     * @Route("/users", name="user_list", methods={"GET"})
      */
-    public function index()
+    public function indexAction()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/IndexController.php',
-        ]);
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+        $encoders = [new XmlEncoder(), new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers, $encoders);
+        $jsonContent =  $serializer->serialize($users,"json");
+        $response = new Response($jsonContent);
+        $response->headers->set("Content-Type", "application/json");
+        return $response;
     }
 }
